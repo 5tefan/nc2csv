51385b0db8876aea25ad1926d9c25923a9b352f2
Stefan Codrescu <ssmmcc1@gmail.com>
2023-09-13 23:25:08 -0600

    adds __main__.py for packaging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nc2csv",
-    version="0.1.0",
+    version="0.1.1",
     description="Dump NetCDF file to CSV",
     author="Stefan Codrescu",
     author_email="ssmmcc1@gmail.com",
